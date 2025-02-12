ca96777efc19381c5a248a1fc08e6d13fe30d73c
iketya <ijetyan1021@gmail.com>
2025-02-12 16:43:46 +0900

    expensesに非同期関数とpostの値を入力した

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,11 +1,25 @@
 import { Hono } from 'hono'
 
 
+type Expenses={
+    id:number,
+    title:string,
+    amount:number,
+}
+
+const fakeExpenses: Expenses[]=[
+    {id:1, title:"Groceries",amount:50},
+    {id:2, title:"Utilities",amount:100},
+    {id:3, title:"Rent",amount:1000},
+];
+
 export const expensesRoute = new Hono()
 
-.get("/", (c) => {
-    return c.json({expenses:[]})
+.get("/", async (c) => {
+    return c.json({expenses:fakeExpenses});
 })
-.post("/", (c) => {
-    return c.json({})
-})
+.post("/", async (c) => {
+    const expenses =await c.req.json()
+    console.log({expenses})
+    return c.json(expenses);
+});
