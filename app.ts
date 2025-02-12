0d0597c643404645715e1db60e1a44de2438d068
iketya <ijetyan1021@gmail.com>
2025-02-12 16:27:21 +0900

    /api/expensesでexpensesがみれるようにした

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import { Hono } from 'hono'
-import { logger } from 'hono/logger'
-
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { expensesRoute } from './routes/expenses';
 const app = new Hono()
 
 app.use(logger())
@@ -8,5 +8,7 @@ app.use(logger())
 app.get("/text", c => {
 return c.json({"message":"test"})
 })
+
+app.route("/api/expenses",expensesRoute)
 
 export default app
