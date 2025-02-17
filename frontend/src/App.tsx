ce615f919ef1fefd1dc2e13d377839a26db689b8
iketya <ijetyan1021@gmail.com>
2025-02-17 14:51:08 +0900

    tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          up
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          up
+        </button>
+        <p>
+        {count}
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+content: [
+    "./src/***.jsx",
+    "./src/***.jsx",
+],
+     theme: {
+         extend: {} 
+        }, 
+plugins: [] 
+};
